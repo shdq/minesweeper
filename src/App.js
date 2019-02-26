223e792b7383b1732769ae4160e0e16ab27011ca
Sergei Cherniaev <sergei@cherniaev.com>
2019-02-26 16:05:39 +0300

    class Field added to handle field initialization

--- src/App.js
+++ src/App.js
@@ -2,36 +2,105 @@ import React, { Component } from "react";
 import Cell from "./components/cellComponent";
 import styled from "styled-components";
 
-const Field = styled.span`
+const Wrapper = styled.span`
   border: 2px solid #b6c3d1;
   display: flex;
   flex-wrap: wrap;
   width: 360px;
 `;
 
-const mines = 10;
-const size = 9;
-const bomb = "💣";
+class Field {
+  constructor(width = 9, height = 9, mines = 10) {
+    this.name = "Field";
+    this.bomb = "💣";
+    this.width = width;
+    this.height = height;
+    this.mines = mines;
+    this.data = Array(this.width * this.height).fill(0);
+  }
 
-class App extends Component {
-  render() {
-    const cells = Array(size * size).fill(0);
-    let planted = mines;
+  width() {
+    return this.width;
+  }
+
+  height() {
+    return this.height;
+  }
+
+  get(i, j) {
+    if (i > this.height - 1 || j > this.width - 1 || i < 0 || j < 0) {
+      // out of range
+      return false;
+    }
+    return this.data[i * this.width + j];
+  }
+
+  plant() {
+    let planted = this.mines;
     while (planted > 0) {
-      for (let i = 0; i < size * size; i++) {
-        if (planted > 0 && cells[i] === 0 && Math.random() <= 0.10) {
-          cells[i] = bomb;
-          planted--;
-          // console.log({ planted });
+      for (let i = 0; i < this.height; i++) {
+        for (let j = 0; j < this.width; j++) {
+          if (
+            planted > 0 &&
+            this.data[i * this.width + j] === 0 &&
+            Math.random() <= 0.1
+          ) {
+            this.data[i * this.width + j] = this.bomb;
+            planted--;
+          }
         }
       }
     }
+  }
 
-    const grid = cells.map((value, index) => (
-      <Cell key={index.toString()} background={index} value={value} />
-    ));
+  evaluate() {
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        let value = 0;
+        if (this.get(i, j) === this.bomb) continue;
 
-    return <Field>{grid}</Field>;
+        // top left
+        if (this.get(i - 1, j - 1) === this.bomb) value++;
+        // top
+        if (this.get(i - 1, j) === this.bomb) value++;
+        // top right
+        if (this.get(i - 1, j + 1) === this.bomb) value++;
+        // left
+        if (this.get(i, j - 1) === this.bomb) value++;
+        // right
+        if (this.get(i, j + 1) === this.bomb) value++;
+        // bottom left
+        if (this.get(i + 1, j - 1) === this.bomb) value++;
+        // bottom
+        if (this.get(i + 1, j) === this.bomb) value++;
+        // bottom right
+        if (this.get(i + 1, j + 1) === this.bomb) value++;
+
+        this.data[i * this.width + j] = value;
+      }
+    }
+  }
+
+  init() {
+    this.plant();
+    this.evaluate();
+  }
+}
+
+class App extends Component {
+  render() {
+    const field = new Field(9, 9, 10);
+    field.init();
+    console.log(field);
+
+    const grid = [];
+    for (let i = 0; i < field.width * field.height; i++) {
+      grid.push(
+        <Cell key={i.toString()} background={i} value={field.data[i]} />
+      );
+    }
+
+    return <Wrapper>{grid}</Wrapper>;
   }
 }
 

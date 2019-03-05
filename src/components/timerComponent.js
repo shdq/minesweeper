1517d54abaafad86cb183f08a7346f886f625ee0
Sergei Cherniaev <sergei@cherniaev.com>
2019-03-05 14:51:47 +0300

    timer component fixed

--- src/components/timerComponent.js
+++ src/components/timerComponent.js
@@ -26,7 +26,7 @@ class Timer extends Component {
   }
 
   render() {
-    if (this.props.mood === "😵" || this.props.mood === "😎")
+    if (this.props.mood !== "🙂")
       clearInterval(this.timerID);
     return <Clock>{this.state.time.toString().padStart(3, "0")}</Clock>;
   }

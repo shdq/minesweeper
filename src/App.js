0f7d7208370d6f4ac8729ab82c687044957a203c
Sergei Cherniaev <sergei@cherniaev.com>
2019-03-25 15:25:24 +0300

    difficulty levels added

--- src/App.js
+++ src/App.js
@@ -25,6 +25,51 @@ const Wrapper = styled.span`
   width: ${props => props.width * 42 + "px"};
 `;
 
+const Difficulty = styled.span`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  font-size: 1.2rem;
+  color: #2e5266;
+  margin-top: 10px;
+`;
+
+const Select = styled.select`
+  display: inline-block;
+  width: auto;
+  font-size: 1rem;
+  font-weight: normal;
+  color: #2e5266;
+  line-height: 1.3;
+  padding: 0.6em 1.4em 0.5em 0.8em;
+  max-width: 100%;
+  box-sizing: border-box;
+  margin: 0;
+  border: 1px solid #aaa;
+  box-shadow: 0 1px 0 1px rgba(0, 0, 0, 0.04);
+  border-radius: 0.3em;
+  -moz-appearance: none;
+  -webkit-appearance: none;
+  appearance: none;
+  background-color: #fff;
+  background-image: url("data:image/svg+xml;charset=US-ASCII,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%22292.4%22%20height%3D%22292.4%22%3E%3Cpath%20fill%3D%22%232E5266%22%20d%3D%22M287%2069.4a17.6%2017.6%200%200%200-13-5.4H18.4c-5%200-9.3%201.8-12.9%205.4A17.6%2017.6%200%200%200%200%2082.2c0%205%201.8%209.3%205.4%2012.9l128%20127.9c3.6%203.6%207.8%205.4%2012.8%205.4s9.2-1.8%2012.8-5.4L287%2095c3.5-3.5%205.4-7.8%205.4-12.8%200-5-1.9-9.2-5.5-12.8z%22%2F%3E%3C%2Fsvg%3E"),
+    linear-gradient(to bottom, #ffffff 0%, #e5e5e5 100%);
+  background-repeat: no-repeat, repeat;
+  background-position: right 0.7em top 50%, 0 0;
+  background-size: 0.65em auto, 100%;
+
+  &:focus {
+    border-color: #aaa;
+    box-shadow: 0 0 1px 3px rgba(59, 153, 252, 0.7);
+    box-shadow: 0 0 0 3px -moz-mac-focusring;
+    outline: none;
+  }
+
+  &::-ms-expand {
+    display: none;
+  }
+`;
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -32,7 +77,7 @@ class App extends Component {
     /*
       easy:   9x9, 10 mines
       medium: 16x16, 40 mines
-      hard:   30x12, 99 mines
+      hard:   30x16, 99 mines
     */
 
     /* initialize the game */
@@ -42,6 +87,7 @@ class App extends Component {
       mood: "🙂", // 🙂😨😎😵
       isOpened: new Set(),
       isFlagged: new Set(),
+      difficulty: "Beginner",
       field: f,
       restarted: 0
     };
@@ -49,6 +95,43 @@ class App extends Component {
     this.handleLeftClick = this.handleLeftClick.bind(this);
     this.openMines = this.openMines.bind(this);
     this.restartGame = this.restartGame.bind(this);
+    this.handleDifficultyChange = this.handleDifficultyChange.bind(this);
+  }
+
+  handleDifficultyChange(event) {
+    let width, height, mines;
+    switch (event.target.value) {
+      case "Beginner":
+        width = 9;
+        height = 9;
+        mines = 10;
+        break;
+      case "Intermediate":
+        width = 16;
+        height = 16;
+        mines = 40;
+        break;
+      case "Advanced":
+        width = 30;
+        height = 16;
+        mines = 99;
+        break;
+      default:
+        width = 9;
+        height = 9;
+        mines = 10;
+        break;
+    }
+    const f = new Field(width, height, mines);
+    f.init();
+    this.setState({
+      mood: "🙂", // 🙂😨😎😵
+      isOpened: new Set(),
+      isFlagged: new Set(),
+      difficulty: event.target.value,
+      field: f,
+      restarted: this.state.restarted + 1
+    });
   }
 
   openMines() {
@@ -224,6 +307,19 @@ class App extends Component {
           <Count flagged={this.state.field.mines - this.state.isFlagged.size} />
         </Panel>
         <Wrapper width={this.state.field.width}>{this.grid}</Wrapper>
+        <Difficulty>
+          <label>
+            Difficulty&nbsp;
+            <Select
+              value={this.state.difficulty}
+              onChange={this.handleDifficultyChange}
+            >
+              <option value="Beginner">Beginner</option>
+              <option value="Intermediate">Intermediate</option>
+              <option value="Advanced">Advanced</option>
+            </Select>
+          </label>
+        </Difficulty>
       </React.Fragment>
     );
   }
